d3eef96368221bda610660721db07753dd06f14c
Von Welch <von@vwelch.com>
2011-09-01 21:50:30 -0400

    Added setup.py install file.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name = "everscript",
+    version = "0.1",
+    packages = [ "everscript" ],
+    scripts = [
+        'scripts/en-diary.py',
+        'scripts/en-search.py',
+        ],
+    install_requires=['appscript >= 1.0'],
+
+    author = "Von Welch",
+    author_email = "von@vwelch.com",
+    description = "A python/appscript/AppleScript interface to EverNote",
+    license = "Apache2",
+)
