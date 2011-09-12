332c90c28798f1d76fcc1c7934a87c2751e62ee6
Von Welch <von@vwelch.com>
2011-09-12 11:26:32 -0400

    ToDos: notebook argument is optional to allow initial empty list.

--- everscript/ToDos.py
+++ everscript/ToDos.py
@@ -6,8 +6,11 @@ class ToDos(Notes):
 
     _item_class = ToDo
 
-    def __init__(self, notebook, search_term=""):
+    def __init__(self, notebook=None, search_term=""):
         self.notebook = notebook
-        Notes.__init__(self, EverNote.find_notes(search_term=search_term,
-                                                 notebook=notebook).notes)
-
+        if notebook:
+            notes = EverNote.find_notes(search_term=search_term,
+                                        notebook=notebook).notes
+        else:
+            notes = []
+        Notes.__init__(self, notes)
