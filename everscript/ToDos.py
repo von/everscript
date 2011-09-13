aca1553a5b80a37ae1bca4190a875bbcbf3e7606
Von Welch <von@vwelch.com>
2011-09-13 09:44:06 -0400

    ToDos: added bin_by_due_date()

--- everscript/ToDos.py
+++ everscript/ToDos.py
@@ -1,5 +1,7 @@
 """Collection of ToDo notes"""
 
+import datetime
+
 from . import EverNote, Notes, ToDo
 
 class ToDos(Notes):
@@ -14,3 +16,29 @@ class ToDos(Notes):
         else:
             notes = []
         Notes.__init__(self, notes)
+
+    def bin_by_due_date(self, soon_days=7):
+        """Return separate ToDos sorted by due date.
+
+        Returns tuple of ToDos: past due, due today, due soon, due later, not due.
+        soon defines soon by number of days."""
+        soon = datetime.timedelta(soon_days)
+        today = datetime.date.today()
+        past_due = ToDos()
+        due_today = ToDos()
+        due_soon = ToDos()
+        due_later = ToDos()
+        not_due = ToDos()
+        for todo in self:
+            due_date = todo.due_date()
+            if due_date is None:
+                not_due.append(todo)
+            elif due_date < today:
+                past_due.append(todo)
+            elif due_date == today:
+                due_today.append(todo)
+            elif due_date - today <= soon:
+                due_soon.append(todo)
+            else:
+                due_later.append(todo)
+        return past_due, due_today, due_soon, due_later, not_due
