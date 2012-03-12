ba6796ac381a50376197a34a476dfaa83fdcd977
Von Welch <von@vwelch.com>
2012-03-12 10:45:15 -0400

    Raise EverNoteException instead of appscript.reference.CommandError

--- everscript/EverNote.py
+++ everscript/EverNote.py
@@ -1,6 +1,7 @@
 """EverNote class"""
 
-from appscript import app
+import appscript
+from contextlib import contextmanager
 
 from Note import Note
 from Notes import Notes
@@ -12,7 +13,7 @@ class EverNote(object):
 
     @classmethod
     def __get_app(cls, app_name="EverNote"):
-        return app(app_name)
+        return appscript.app(app_name)
 
     @classmethod
     def create_note(cls, with_html=None, with_text=None, title="", notebook=None):
@@ -25,7 +26,9 @@ class EverNote(object):
             kwargs["with_html"] = with_html
         if with_text is not None:
             kwargs["with_text"] = with_text
-        return Note(cls.__get_app().create_note(**kwargs))
+        with AppCallContextManager():
+            note = cls.__get_app().create_note(**kwargs)
+        return Note(note)
 
     @classmethod
     def find_notes(cls, search_term, notebook=None):
@@ -36,7 +39,9 @@ class EverNote(object):
         search_term="\"" + search_term + "\""
         if notebook:
             search_term += " notebook:\"{}\"".format(notebook)
-        return Notes(cls.__get_app().find_notes(search_term))
+        with AppCallContextManager():
+            notes = cls.__get_app().find_notes(search_term)
+        return Notes(notes)
 
     @classmethod
     def open_collection_window(cls, query_string=None):
@@ -45,10 +50,32 @@ class EverNote(object):
         kwargs={}
         if query_string:
             kwargs["with_query_string"] = query_string
-        return cls.__get_app().open_collection_window(**kwargs)
+        with AppCallContextManager():
+            window = cls.__get_app().open_collection_window(**kwargs)
+        return window
 
     @classmethod
     def open_note_window(cls, note):
-        return cls.__get_app().open_note_window(with_=note.note)
+        with AppCallContextManager():
+            window = cls.__get_app().open_note_window(with_=note.note)
+        return window
+
+class EverNoteException(Exception):
+    def __init__(self, message, detailed_message):
+        self.message = message
+        self.detailed_message = detailed_message
+
+    def __str__(self):
+        return self.message
+
+class AppCallContextManager:
+    """Context manager for calls to appscript app"""
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, tb):
+        if type == appscript.reference.CommandError:
+            raise EverNoteException(value.errormessage,
+                                    str(value))
 
         
