098c141045b5fb80f24c8d6e8543469d2c929d76
Von Welch <von@vwelch.com>
2013-01-21 15:56:05 -0500

    en-diary.py: Basic working version of what was in evernote.py

--- scripts/en-diary.py
+++ scripts/en-diary.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 """Evernote diary manager
 """
-from appscript import app
 import argparse
+import ConfigParser
 from datetime import date
 import logging
+import os
 import sys
 
 from everscript import EverNote
@@ -44,21 +45,71 @@ def main(argv=None):
 				 action="store_const", const=logging.WARNING,
 				 dest="output_level",
 				 help="run quietly")
+    parser.add_argument("-c", "--config",
+			default="~/.evernote/config",
+			help="specify configuration file")
+    parser.add_argument("-f", "--force",
+			action='store_const', const=True,
+			dest="force", default=False,
+			help="Force creation of new diary")
     parser.add_argument("--version", action="version", version="%(prog)s 1.0")
     args = parser.parse_args()
     output_handler.setLevel(args.output_level)
 
-    todays_title = date.today().strftime("%B %d, %Y")
-    todays_notes = EverNote.find_notes(todays_title, notebook=DIARY_NOTEBOOK)
-    if len(todays_notes):
-	output.info("Opening existing diary for {}".format(todays_title))
-	todays_note = todays_notes[0]
+    config = ConfigParser.SafeConfigParser()
+    conf_path = os.path.expanduser(args.config)
+    if os.path.exists(conf_path):
+	output.debug("Parsing configuration file {}".format(args.config))
+	config.read(conf_path)
+
+    title = date.today().strftime("%B %d, %Y")
+    output.debug("Today's note title is: {}".format(title))
+
+    try:
+	notebook = config.get("Diary", "Notebook")
+    except (ConfigParser.NoOptionError,
+	    ConfigParser.NoSectionError) as e:
+	output.error("No Diary notebook defined in configuration")
+	sys.exit(1)
+
+    if args.force:
+        output.debug("Forcing creationg of new diary")
+        note = None
     else:
-	output.info("Creating new diary for {}".format(todays_title))
-	todays_note = EverNote.create_note(with_text="",
-					   title=todays_title,
-					   notebook=DIARY_NOTEBOOK)
-    EverNote.open_note_window(todays_note)
+        try:
+            note = EverNote.find_note_by_title(title, notebook=notebook)
+        except EverNoteException as e:
+            output.exception("Error trying to find today's diary")
+            raise
+
+    if note:
+	output.debug("Using existing diary: {}".format(title))
+    else:
+        output.debug("Creating new diary: {}".format(title))
+        try:
+            template_title = config.get("Diary", "Template")
+            output.debug("Using template: {}".format(template_title))
+            template_note = EverNote.find_notes(template_title,
+                                                notebook=notebook)[0]
+            template = template_note.content()
+        except (ConfigParser.NoOptionError,
+                ConfigParser.NoSectionError) as e:
+            pass  # No template defined
+        except EverNoteException as e:
+            output.exception("Error finding diary template")
+            sys.exit(1)
+
+        html = template.format(events="", todo="")
+        try:
+            note = EverNote.create_note(with_html=html,
+                                        title=title,
+                                        notebook=notebook)
+        except EverNoteException as e:
+            output.exception("Error creating today's diary")
+            sys.exit(1)
+
+    output.debug("Success. Opening diary: {}".format(title))
+    EverNote.open_note_window(note)
     return(0)
 
 if __name__ == "__main__":
